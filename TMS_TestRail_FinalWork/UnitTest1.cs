4bb4d096b2da474b98278e8349b3563984929e45
Nikolajml <nicolas.maliavko@gmail.com>
2023-06-25 13:55:51 +0300

    Add project files.

---
+++ TMS_TestRail_FinalWork/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TMS_TestRail_FinalWork
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
