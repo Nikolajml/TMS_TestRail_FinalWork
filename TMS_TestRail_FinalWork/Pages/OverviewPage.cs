54a2c352e2b21bfa84ea768b87c7d171463c733c
Nikolajml <nicolas.maliavko@gmail.com>
2023-06-29 13:35:45 +0300

    Created entity deletion test

---
+++ TMS_TestRail_FinalWork/Pages/OverviewPage.cs
@@ -0,0 +1,44 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMS_TestRail_FinalWork.Core;
+
+namespace TMS_TestRail_FinalWork.Pages
+{
+    public class OverviewPage : BasePage
+    {
+        private static string END_POINT = "index.php?/admin/projects/add";
+
+        private static readonly By ProjectsButtonBy = By.Id("navigation-sub-projects");
+        
+        public OverviewPage(IWebDriver? driver, bool openPageByUrl) : base(driver, openPageByUrl)
+        {
+
+        }
+
+        public OverviewPage(IWebDriver? driver) : base(driver, false)
+        {
+
+        }
+
+        public override bool IsPageOpened()
+        {
+            return WaitService.GetVisibleElement(ProjectsButtonBy) != null;
+        }
+
+        protected override string GetEndpoint()
+        {
+            return END_POINT;
+        }
+
+        public ProjectsPage NavigateToProjectsPage()
+        {
+            Driver.FindElement(ProjectsButtonBy).Click();
+            return new ProjectsPage(Driver);
+        }
+
+    }
+}
--- TMS_TestRail_FinalWork/Pages/ProjectsPage.cs
+++ TMS_TestRail_FinalWork/Pages/ProjectsPage.cs
@@ -1,4 +1,5 @@
 ﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,7 +13,10 @@ namespace TMS_TestRail_FinalWork.Pages
     {
         private static string END_POINT = "index.php?/admin/projects/overview";
                 
-        private static readonly By SuccessMessageBy = By.ClassName("message-success");
+        private static readonly By MessageBy = By.ClassName("message-success");
+        private static readonly By DeleteIconBy = By.ClassName("icon-small-delete");
+        private static readonly By DeleteCheckboxBy = By.XPath("//span[@class='dialog-confirm-busy']/following::input");
+        private static readonly By OkButtonBy = By.CssSelector(".ui-dialog .dialog .dialog-buttons-highlighted .dialog-action-default");
 
         public ProjectsPage(IWebDriver? driver, bool openPageByUrl) : base(driver, openPageByUrl)
         {
@@ -26,7 +30,7 @@ namespace TMS_TestRail_FinalWork.Pages
 
         public override bool IsPageOpened()
         {
-            return WaitService.GetVisibleElement(SuccessMessageBy) != null;
+            return WaitService.GetVisibleElement(MessageBy) != null;
         }
 
         protected override string GetEndpoint()
@@ -34,9 +38,37 @@ namespace TMS_TestRail_FinalWork.Pages
             return END_POINT;
         }            
 
-        public string GetSuccessMessage()
+        public string GetSuccessCreatedMessage()
         {
-            return Driver.FindElement(SuccessMessageBy).Text;
+            return Driver.FindElement(MessageBy).Text;
+        }
+
+        public string GetSuccessDeletedMessage()
+        {
+            return Driver.FindElement(MessageBy).Text;
+        }               
+
+        void ClickDeletIcon()
+        {
+            Driver.FindElement(DeleteIconBy).Click();
+        }
+
+        void ClickDeleteProjectCheckBox()
+        {
+            Driver.FindElement(DeleteCheckboxBy).Click();
+        }
+
+        void ClickOkButton()
+        {
+            Driver.FindElement(OkButtonBy).Click(); 
+        }
+
+        public ProjectsPage DeleteProject()
+        {
+            ClickDeletIcon();
+            ClickDeleteProjectCheckBox();
+            ClickOkButton();
+            return this;
         }
     }
 }
