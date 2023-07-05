e66735e2d5da836100de629362494e8627340dc9
Nikolajml <nicolas.maliavko@gmail.com>
2023-07-05 16:21:01 +0300

    Correct Limit Value Test

--- TMS_TestRail_FinalWork/Tests/UI/Positive tests/LimitValueTest.cs
+++ TMS_TestRail_FinalWork/Tests/UI/Positive tests/LimitValueTest.cs
@@ -17,10 +17,11 @@ namespace TMS_TestRail_FinalWork.Tests.Positive_tests
         [Description("The number of entered values does not exceed 250")]
         [AllureOwner("User")]
         [AllureTag("Smoke")]
-        [SmokeTest]
-        [TestCase("The number of entered values is 34")]        
-        public void NotExceedingLimitValuesTest(string searchProject)
-        {           
+        [SmokeTest]        
+        public void NotExceedingLimitValuesTest()
+        {
+            string searchProject = "The number of entered values is 34";
+
             User user = new UserBuilder()
                 .SetUsername("nicolas.maliavko@gmail.com")
                 .SetPassword("Qwer_1234")
@@ -33,14 +34,15 @@ namespace TMS_TestRail_FinalWork.Tests.Positive_tests
             Assert.IsFalse(dashboard_SearchProject_Page.IsPresentErrorLimitDialogMessage());
         }
 
-        [Test(Description = "Limit value test")]
+        [Test(Description = "Limit value test")]        
         [Description("The number of entered values is 250")]
         [AllureOwner("User")]
         [AllureTag("Smoke")]
-        [SmokeTest]
-        [TestCase("The number of entered 250The number of entered 250The number of entered 250The number of entered 250The number of entered 250The number of entered 250The number of entered 250The number of entered 250The number of entered 250The number of entered 250")]
-        public void NumberOfValuesEqualsBoundsTest(string searchProject)
+        [SmokeTest]        
+        public void NumberOfValuesEqualsBoundsTest()
         {
+            string searchProject = "The number of entered 250The number of entered 250The number of entered 250The number of entered 250The number of entered 250The number of entered 250The number of entered 250The number of entered 250The number of entered 250The number of entered 250";
+
             User user = new UserBuilder()
                 .SetUsername("nicolas.maliavko@gmail.com")
                 .SetPassword("Qwer_1234")
@@ -58,10 +60,11 @@ namespace TMS_TestRail_FinalWork.Tests.Positive_tests
         [Description("The number of entered values exceed 250")]
         [AllureOwner("User")]
         [AllureTag("Smoke")]
-        [SmokeTest]
-        [TestCase("The number of entered measurements exceed 250 / The number of entered measurements exceed 250 / The number of entered measurements exceed 250 / The number of entered measurements exceed 250 / The number of entered measurements exceed 250 / The number of entered measurements exceed 250 /")]
-        public void ExceedingLimitValuesTest(string searchProject)
-        {       
+        [SmokeTest]       
+        public void ExceedingLimitValuesTest()
+        {
+            string searchProject = "The number of entered measurements exceed 250 / The number of entered measurements exceed 250 / The number of entered measurements exceed 250 / The number of entered measurements exceed 250 / The number of entered measurements exceed 250 / The number of entered measurements exceed 250 /";
+
             User user = new UserBuilder()
                 .SetUsername("nicolas.maliavko@gmail.com")
                 .SetPassword("Qwer_1234")
