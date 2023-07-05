55791161c6e85f133e1db875140fb6806fe6411d
Nikolajml <nicolas.maliavko@gmail.com>
2023-07-05 17:45:00 +0300

    Redid Boudary Values Test

---
+++ TMS_TestRail_FinalWork/Tests/UI/Positive tests/BoundaryValuesTest/BoundaryValueEqualsTest.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Allure.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMS_TestRail_FinalWork.BaseEntities;
+using TMS_TestRail_FinalWork.Models;
+
+namespace TMS_TestRail_FinalWork.Tests.UI.Positive_tests.BoundaryValuesTest
+{
+    public class BoundaryValueEqualsTest : BaseTest
+    {
+        [Test(Description = "Limit value test")]
+        [Category("Boundary Value Test")]
+        [Description("The number of entered values is 250")]
+        [AllureOwner("User")]
+        [AllureTag("Smoke")]
+        [SmokeTest]
+        public void BoundaryValeuEquals_250_Test()
+        {
+            string searchProject = "The number of entered 250The number of entered 250The number of entered 250The number of entered 250The number of entered 250The number of entered 250The number of entered 250The number of entered 250The number of entered 250The number of entered 250";
+
+            User user = new UserBuilder()
+                .SetUsername("nicolas.maliavko@gmail.com")
+                .SetPassword("Qwer_1234")
+                .Build();
+
+            LoginPage.SuccessfulLogin(user)
+                .NavigateToProjectCategorySearch()
+                .EnterDataInSearchField(searchProject);
+
+            Assert.IsFalse(dashboard_SearchProject_Page.IsPresentErrorLimitDialogMessage());
+        }
+    }
+}
---
+++ TMS_TestRail_FinalWork/Tests/UI/Positive tests/BoundaryValuesTest/BoundaryValueMoreTest.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Allure.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMS_TestRail_FinalWork.BaseEntities;
+using TMS_TestRail_FinalWork.Models;
+
+namespace TMS_TestRail_FinalWork.Tests.UI.Positive_tests.BoundaryValuesTest
+{
+    public class BoundaryValueMoreTest : BaseTest
+    {
+        [Test(Description = "Limit value test")]
+        [Category("Boundary Value Test")]
+        [Description("The number of entered values exceed 250")]
+        [AllureOwner("User")]
+        [AllureTag("Smoke")]
+        [SmokeTest]
+        public void BoundaryValeuMore_250_Test()
+        {
+            string searchProject = "The number of entered measurements exceed 250 / The number of entered measurements exceed 250 / The number of entered measurements exceed 250 / The number of entered measurements exceed 250 / The number of entered measurements exceed 250 / The number of entered measurements exceed 250 /";
+
+            User user = new UserBuilder()
+                .SetUsername("nicolas.maliavko@gmail.com")
+                .SetPassword("Qwer_1234")
+                .Build();
+
+            LoginPage.SuccessfulLogin(user)
+                .NavigateToProjectCategorySearch()
+                .EnterDataInSearchField(searchProject)
+                .WaitDialogWindow();
+
+            Assert.IsTrue(dashboard_SearchProject_Page.IsPresentErrorLimitDialogMessage());
+        }
+    }
+}
