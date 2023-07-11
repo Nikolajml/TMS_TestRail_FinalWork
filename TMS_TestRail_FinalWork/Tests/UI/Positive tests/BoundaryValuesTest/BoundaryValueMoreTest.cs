042fba8d32849793ded2fe67f1b57b57eef5fec7
Nikolajml <nicolas.maliavko@gmail.com>
2023-07-11 13:21:19 +0300

    Added and corrected boundary valeus tests

--- TMS_TestRail_FinalWork/Tests/UI/Positive tests/BoundaryValuesTest/BoundaryValueMoreTest.cs
+++ TMS_TestRail_FinalWork/Tests/UI/Positive tests/BoundaryValuesTest/BoundaryValueMoreTest.cs
@@ -11,15 +11,15 @@ namespace TMS_TestRail_FinalWork.Tests.UI.Positive_tests.BoundaryValuesTest
 {
     public class BoundaryValueMoreTest : BaseTest
     {
-        [Test(Description = "Limit value test")]
+        [Test(Description = "Boundary value test")]
         [Category("Boundary Value Test")]
-        [Description("The number of entered values exceed 250")]
+        [Description("The number of entered values is 251")]
         [AllureOwner("User")]
         [AllureTag("Smoke")]
         [SmokeTest]
         public void BoundaryValeuMore_250_Test()
         {
-            string searchProject = "The number of entered measurements exceed 250 / The number of entered measurements exceed 250 / The number of entered measurements exceed 250 / The number of entered measurements exceed 250 / The number of entered measurements exceed 250 / The number of entered measurements exceed 250 /";
+            string searchProject = "The number of entered measurements exceed 250 / The number of entered measurements exceed 250 / The number of entered measurements exceed 250 / The number of entered measurements exceed 250 / The number of entered measurements exceed 250 / The numbers";
 
             User user = new UserBuilder()
                 .SetUsername("nicolas.maliavko+1@gmail.com")
